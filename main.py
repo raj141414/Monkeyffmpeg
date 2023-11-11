eb46e8e3fb7e8284b4d3382a79638ecc26c2c6f8
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-11-11 09:56:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ bot = Client(
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hello [{m.from_user.first_name}] Welcome to TXT Downloader Bot!(tg://user?id={m.from_user.id})\nPress /tor")
+    editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /tor")
 
 
 @bot.on_message(filters.command("restart"))
@@ -44,7 +44,7 @@ async def restart_handler(_, m):
 
 @bot.on_message(filters.command(["tor"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text('Send links listed in a txt file in format **Name:link')
+    editable = await m.reply_text('Send TXT file for download')
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
